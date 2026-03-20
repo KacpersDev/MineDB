8bb56932cb1684d8e953ea692b397fad26b1a5f7
kacper <vkacperpl@gmail.com>
2026-03-20 15:17:52 +0100

    InventoryComponent

--- src/main/java/dev/minedb/components/impl/InventoryComponent.java
+++ src/main/java/dev/minedb/components/impl/InventoryComponent.java
@@ -10,11 +10,34 @@ public class InventoryComponent implements Component<ItemStack[]> {
 
     @Override
     public String serialize(ItemStack @NotNull [] itemStacks) {
-        return null;
+        StringBuilder serialized = new StringBuilder();
+        for (int i = 0; i < itemStacks.length; i++) {
+            if (itemStacks[i] == null) continue;
+            String item = itemStackComponent.serialize(itemStacks[i]);
+            String index = i + "§" + item;
+
+            if (serialized.isEmpty()) {
+                serialized = new StringBuilder(index);
+            } else {
+                serialized.append(";").append(index);
+            }
+        }
+
+        return serialized.toString();
     }
 
     @Override
     public ItemStack[] deserialize(@NotNull String s) {
-        return new ItemStack[0];
+        ItemStack[] content = new ItemStack[54];
+
+        for (final String originalSplit : s.split(";")) {
+            String[] split = originalSplit.split("§");
+
+            int index = Integer.parseInt(split[0]);
+            ItemStack itemStack = itemStackComponent.deserialize(split[1]);
+            content[index] = itemStack;
+        }
+
+        return content;
     }
 }
